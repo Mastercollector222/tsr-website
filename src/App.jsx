81ef47fb7310fa26bf18ebec56f0d78c7082a798
Mastercollector222 <topshotrewards@gmail.com>
2025-02-13 14:09:07 -0700

    Fix EVMToken import case to match actual file name

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Account from './pages/Account';
 import FlowToken from './pages/FlowToken';
-import EVMToken from './pages/EVMToken';
+import EvmToken from './pages/EvmToken';
 import About from './pages/About';
 import Community from './pages/Community';
 import Roadmap from './pages/Roadmap';
@@ -24,7 +24,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/account" element={<Account />} />
                 <Route path="/flow-token" element={<FlowToken />} />
-                <Route path="/evm-token" element={<EVMToken />} />
+                <Route path="/evm-token" element={<EvmToken />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/community" element={<Community />} />
                 <Route path="/roadmap" element={<Roadmap />} />
